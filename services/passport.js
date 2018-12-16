763c16d941855e2b4b6977f3bdf9e9a77a271f94
gajendra <gajendra.n@pacewisdom.in>
2018-12-16 23:55:29 +0530

    google strategy proxy issue

--- services/passport.js
+++ services/passport.js
@@ -17,26 +17,23 @@ passport.deserializeUser((id, done) => {
 });
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-      proxy: true,
-    },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if(existingUser) {
-            done(null, existingUser);     //first arg is error and second is user
-          } else {
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user));
-          }
-        })
-    }
-  )
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    proxy: true
+  }, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        if(existingUser) {
+          done(null, existingUser);     //first arg is error and second is user
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      })
+  })
 );
 
 /* the call back function here in the passport.use(strategy, *callback*) is the function that gets called
