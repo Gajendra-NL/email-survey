60fe20bed0707bda626dd1cfbbc436cf2709ce67
gajendra <gajendra.n@pacewisdom.in>
2018-12-26 22:45:53 +0530

    authentication client side, header design

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = (props) => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Email-Survey
+      </h1>
+      Collect feedback from customers
+    </div>
+  )
+}
+
+export default Landing;
--- services/passport.js
+++ services/passport.js
@@ -22,17 +22,14 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-  }, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-      .then((existingUser) => {
-        if(existingUser) {
-          done(null, existingUser);     //first arg is error and second is user
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      })
+  }, async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id });
+    if(existingUser) {
+      done(null, existingUser);     //first arg is error and second is user
+    } else {
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user);
+    }
   })
 );
 
